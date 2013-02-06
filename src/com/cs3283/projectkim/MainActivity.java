844bf154a6240dc0f8930865a24181b4b789a6ca
Shaun Lee <lee.jwsh@gmail.com>
2013-02-06 15:33:26 +0800

    Remove unused import

--- src/com/cs3283/projectkim/MainActivity.java
+++ src/com/cs3283/projectkim/MainActivity.java
@@ -5,15 +5,7 @@ import android.app.Activity;
 import android.os.Handler; 
 import android.content.Intent;
 import android.view.Menu;
-//import android.view.Window;
-//import android.view.WindowManager;
-//import android.app.AlertDialog;
 import android.app.Dialog;
-//import android.content.DialogInterface;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.Toast;
-//import android.util.Log;
 
 public class MainActivity extends Activity
 {
